6e9a81dbbe1c897a05a9fc1af461962f2efb232c
unknown <ibrahimm.yusuf2000@gmail.com>
2026-05-03 19:25:11 +0700

    Fix Streamlit app UI and backend integration

--- frontend/app_ui.py
+++ frontend/app_ui.py
@@ -21,8 +21,7 @@ st.sidebar.title("ℹ️ About")
 st.sidebar.info(
     "This system estimates cardiovascular risk using machine learning "
     "and provides explainable insights to support clinical decision making.\n\n"
-    "This AI Project is created" 
-    "by Ibrahim Muhammad Yusuf \n\n"
+    "This AI Project is created by Ibrahim Muhammad Yusuf \n\n"
     "@2026 All Rights Reserved"
 )
 
